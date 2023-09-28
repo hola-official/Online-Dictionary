3dc49d662fa341efced7d0e61bf9265a28c16a06
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-09-28 11:50:47 +0100

    git update

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
-// btn.addEventListener("click", () => {
-//     fetch(url).then((response => response.json()))
-// })
+
 
 btn.addEventListener('click', () => {
     const input = document.getElementById('input-word').value;
